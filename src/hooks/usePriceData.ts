5f25447dbcf3d3e1989cec2ef3d83b545f76cdf5
David Fu <davidzwfu@gmail.com>
2025-03-10 20:29:37 -0400

    refresh prices once per minute to reduce API calls

--- src/hooks/usePriceData.ts
+++ src/hooks/usePriceData.ts
@@ -6,7 +6,7 @@ export function usePriceData() {
   const { data } = useSWR(
     `https://rest.coincap.io/v3/assets?ids=${Object.keys(chains).join(',')}&apiKey=02a2068bea0c40706cb909555c0794381a934777993f14115b9e8da7073902a2`, 
     fetcher,
-    { refreshInterval: 5000 },
+    { refreshInterval: 60000 },
   )
   if (!data || !Array.isArray(data.data))
     return {}
